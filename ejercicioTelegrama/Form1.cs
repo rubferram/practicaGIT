e413b4bfbab1ae8c3e276fcc7a64888a3b30bbaf
rubferram <rubferram@alu.edu.gva.es>
2026-09-19 19:19:30 +0200

    Añado código local con errores

--- ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/Form1.cs
@@ -1,9 +1,54 @@
-namespace ejercicioTelegrama;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-public partial class Form1 : Form
+namespace WindowsFormsApp1
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+
+            //Leo el telegrama 
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente?
+            if (cbUrgente.Checked)
+                tipoTelegrama = 'u';
+            //Obtengo el número de palabras que forma el telegrama numPalabras = textoTelegrama.Length;
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+                if (numPalabras <= 10)
+                    coste = 25;
+                else
+                    coste = 0.5 * numPalabras;
+            else
+            //Si el telegrama es urgente
+            if (tipoTelegrama == 'u')
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 5 + 0.75 * (numPalabras - 10);
+            else
+                coste = 0;
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
     }
 }
