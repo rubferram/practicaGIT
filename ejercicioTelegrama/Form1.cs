1e53adc16360562ce2962681af1352514e685afd
DanielDASAedu <244758806+DanielDASAedu@users.noreply.github.com>
2026-09-19 19:55:50 +0200

    Formulario corregido

--- ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/Form1.cs
@@ -22,32 +22,54 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string textoTelegrama;
-            char tipoTelegrama = ' ';
+
+            string textoTelegrama ="";
+            char tipoTelegrama = 'o';
             int numPalabras = 0;
             double coste;
 
             //Leo el telegrama 
             textoTelegrama = txtTelegrama.Text;
             // telegrama urgente?
-            if (cbUrgente.Checked)
+            if (chkUrgente.Checked)
+            {
                 tipoTelegrama = 'u';
-            //Obtengo el número de palabras que forma el telegrama numPalabras = textoTelegrama.Length;
+            }
+            //Obtengo el número de palabras que forma el telegrama 
+            string[] palabras = textoTelegrama.Split(' ');
+            numPalabras = palabras.Length;
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
+            {
                 if (numPalabras <= 10)
-                    coste = 25;
+                {
+                    coste = 2.5;
+                }
                 else
-                    coste = 0.5 * numPalabras;
+                {
+
+                    coste = ((numPalabras - 10) * 0.5 ) + 2.5;
+                }
+            }
             else
             //Si el telegrama es urgente
-            if (tipoTelegrama == 'u')
-                if (numPalabras <= 10)
-                    coste = 5;
+            {
+                if (tipoTelegrama == 'u')
+                {
+                    if (numPalabras <= 10)
+                    {
+                        coste = 5;
+                    }
+                    else
+                    {
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                    }
+                }
                 else
-                    coste = 5 + 0.75 * (numPalabras - 10);
-            else
-                coste = 0;
+                {
+                    coste = 0;
+                }
+            }
             txtPrecio.Text = coste.ToString() + " euros";
         }
     }
